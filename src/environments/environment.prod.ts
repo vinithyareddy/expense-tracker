a3c005f78d258e3149532c464b591af942bb03b9
vinithya <vinithyareddyp.com>
2025-04-18 11:47:11 -0400

    commit-56

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebaseConfig: {
+      apiKey: "AIzaSyBBSv-o7BOA93UUT9RxszDM276jXti61bk",
+      authDomain: "vp-expense-tracker.firebaseapp.com",
+      projectId: "vp-expense-tracker",
+      storageBucket: "vp-expense-tracker.firebasestorage.app",
+      messagingSenderId: "211072673901",
+      appId: "1:211072673901:web:17450d77ac0d6c170c7764"
+    }
+  };
+  
